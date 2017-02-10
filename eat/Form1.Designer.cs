22b018432828dbb4600885fbcb378f038bdaaff6
stevenyu113228 <stevenyu113228@gmail.com>
2017-02-10 16:30:22 +0800

    第一版程式碼

---
+++ eat/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace eat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(52, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "一餐";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(106, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 16);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "三餐";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(52, 34);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(48, 16);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "教餐";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(106, 34);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(48, 16);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "後餐";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "全選 / 取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "今天要ㄘ：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(94, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(31, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "ㄘ什麼?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 144);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "ㄘ什麼? V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ eat/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eat
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        private bool[] prefer = new bool[4];
+        private int din = 87;
+        private string restaurant = "";
+        private  string[,] rest = new string[4,15] 
+        {
+            {"摩斯","美而美","嚴茶","韓風小舖","四海遊龍","上品排骨","台灣古早味","安娜的廚房","自助餐","地中海","鐵板","丼太郎","李媽媽","全家","7-11"},
+            {"7-11","麵包店","美而美","豪享來","饗初滷味","燒臘","八方雲集","自助餐","日和", "藝素佳","蔥抓餅","阿水","","",""},
+            {"自助餐","燒臘","","","","","","","","","","","","",""},
+            {"炒飯","炒麵","","","","","","","","","","","","",""}
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("ㄤㄤ\n今天尼要ㄘ什麼ㄋ?","ㄘ什麼 V1.0",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+        }
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+                prefer[3] = true;
+            else
+                prefer[3] = false;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+                prefer[2] = true;
+            else
+                prefer[2] = false;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+                prefer[1] = true;
+            else
+                prefer[1] = false;
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                prefer[0] = true;
+            else
+                prefer[0] = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+            }
+            else
+            {
+                checkBox1.Checked = true;
+                checkBox2.Checked = true;
+                checkBox3.Checked = true;
+                checkBox4.Checked = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            return;
+        }
+        private void getRest()
+        {
+            din = rand.Next(0, 4);
+            while (prefer[din] != true)
+            {
+                din = rand.Next(0, 4);
+            }
+            switch (din)
+            {
+                case 0:
+                    label2.Text = checkBox1.Text;
+                    restaurant = checkBox1.Text;
+                    break;
+                case 1:
+                    label2.Text = checkBox2.Text;
+                    restaurant = checkBox2.Text;
+                    break;
+                case 2:
+                    label2.Text = checkBox3.Text;
+                    restaurant = checkBox3.Text;
+                    break;
+                case 3:
+                    label2.Text = checkBox4.Text;
+                    restaurant = checkBox4.Text;
+                    break;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            bool ck = false;
+            for (int i = 0; i < 4; i++)
+            {
+                if (prefer[i])
+                {
+                    ck = true;
+                }
+            }
+            if (ck == false)
+            {
+                MessageBox.Show("87\n你沒有勾選", "8787", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            getRest();
+            /*
+            if (din == 87)
+            {
+                MessageBox.Show("請先選擇餐廳!!","87忘記選餐廳",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return ;
+            }
+            */
+            int choose = 0;
+            switch(din)
+            {
+                case 0:
+                    choose = rand.Next(0, 15);
+                    break;
+                case 1:
+                    choose = rand.Next(0, 12);
+                    break;
+                case 2:
+                    choose = rand.Next(0, 2);
+                    break;
+                case 3:
+                    choose = rand.Next(0, 2);
+                    break;
+            }
+            label2.Text += " ";
+            label2.Text += rest[din, choose];
+            MessageBox.Show(string.Format("今天ㄘ{0}的{1}", restaurant, rest[din,choose]),"ㄘㄉ東西",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+        }
+    }
+}
